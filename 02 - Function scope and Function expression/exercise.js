61b1fd94a36221c5ea31f2094abd36d098c91aa2
Sunil Kathuria <sunilkathuria@gmail.com>
2024-07-23 13:12:44 +0530

    Update exercise.js
    added comment and language correction

--- 02 - Function scope and Function expression/exercise.js
+++ 02 - Function scope and Function expression/exercise.js
@@ -1,21 +1,21 @@
 // Arguments object
 
-// Write a function that prints the values of "arguments" object. Call the function with 5 arguments.
+// Write a function that prints the values of the "arguments" object. Call the function with 5 arguments.
 function showArgumentsObject(){
     console.log(arguments)
 }
 
 showArgumentsObject("This", "function", "gets", 5, "arguments")
 
-// Write a function that accpets one parameter. Print the values of the areguments obhect. Call the 
-// function with 5 arguments. Observe the values of arguments object.
+// Write a function that accepts one parameter. Print the values of the argument object. Call the 
+// function with 5 arguments. Observe the values of the argument object.
 function fucnWithParamAndShowsArgumentsObject(param){
     console.log(param)
     console.log(arguments)
 }
 fucnWithParamAndShowsArgumentsObject("This", "function", "gets", 5, "arguments")
 
-// Write a function to add all the numbers passed to a fucntion using arguments object
+// Write a function to add all the numbers passed to a function using the arguments object
 function addNumbers(){
     let result = 0
     for(num of arguments){
@@ -25,7 +25,7 @@ function addNumbers(){
 }
 console.log(addNumbers(1, 2, 3, 4, 5))
 
-// Write a function that returns a maximum value from all the numbers passed to it. Use arguments object
+// Write a function that returns a maximum value from all the numbers passed. Use arguments object
 function findMax(){
     let maxNum = 0
     for (num of arguments){
@@ -37,10 +37,10 @@ function findMax(){
 }
 console.log(findMax(2, 5, 1, 6, 3, 1, 9, 4, 2))
 
-// How an array is different from an arguments object. State the differences
+// How is an array different from an argument object? State the differences
 
 // Scope of variables
-// Write a function "diffVariables" that creates 4 varibles using keyword "let", "const", "var" and no 
+// Write a function "diffVariables" that creates 4 variables using keywords "let", "const", "var" and no 
 // keyword. Debug the code and observe how these variables are declared and when these are initialized.
 function diffVariables(){
     let x = 3
@@ -49,8 +49,8 @@ function diffVariables(){
     a = 6
 }
 
-// Write a function "diffVariablesWithBlock" that creates 4 varibles using keyword "let", "const", "var" 
-// and no keyword. Create a block within the function and decalare anoter set of variables using the above
+// Write a function "diffVariablesWithBlock" that creates 4 variables using keywords "let", "const", "var" 
+// and no keyword. Create a block within the function and declare another set of variables using the above
 // keywords. Debug the code and observe how these variables are declared and when these are initialized.
 function diffVariablesWithBlock(){
     let x = 3
@@ -65,23 +65,23 @@ function diffVariablesWithBlock(){
     }
 }
 
-// While debugging the function "diffVariablesWithBlock" observe which all variables are hoisted.
+// While debugging the function "diffVariablesWithBlock," observe which variables are hoisted.
 
-// Declare a few variables outside funtion an block scope. Use differnt keywords while declaring these 
-// variables. Write a function "showGlobalData" which prints these variables. 
-// Debug the function and observe when a closure is visible under "Varibale" during debug sessions
+// Declare a few variables outside the function of a block scope. Use different keywords while declaring these 
+// variables. Write a function called "showGlobalData," which prints these variables. 
+// Debug the function and observe when a closure is visible under "Variable" during debug sessions
 let x = 3
 const y = 4
 var z = 5
 a = 6
 function showGlobalData(){
     let x1 = 30
-    console.log(x1)
+    console.log(x1) // replace the variable with different variables above and observe the behavior.
 }
 showGlobalData()    
 
 // function expression
-// Write a function exprsssion that accpets two parameters and returns sum of the two numbers
+// Write a function expression that accepts two parameters and returns the sum of the two numbers
 let addTwoNumbers = function (x, y){
     return x + y
 }
