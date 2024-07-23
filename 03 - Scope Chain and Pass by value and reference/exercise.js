c4d3b1487eebfb938140448d3e31fcfecd39981b
Sunil Kathuria <sunilkathuria@gmail.com>
2024-07-23 18:47:28 +0530

    Update exercise.js
    language changes

--- 03 - Scope Chain and Pass by value and reference/exercise.js
+++ 03 - Scope Chain and Pass by value and reference/exercise.js
@@ -1,6 +1,6 @@
 // Hoisting
 // Use the below source code and change the variable defintion using different keywords and
-// without using the keyword. Observe the behaviour. Recall why this happens.
+// without using the keyword. Observe the behavior. Recall why this happens.
 function checkHoisting(){
     console.log(x)
     const x = 10
@@ -15,13 +15,13 @@ function testVarScope(num){
     num = 100
 }
 // testVarScope(num)
-// // Guess the expected outupt and why?
+// // Guess what the expected output is and why.
 // console.log(num) // What is the expected output
 
 // 
 function testVarScope_1(){
     for (let counter = 0; counter < 10; counter++);
-    console.log(counter) // What is the expected output?
+    console.log(counter) // Guess what the expected output is.
 }
 // testVarScope_1()
 
@@ -31,19 +31,19 @@ function testVarScope_2(){
     {
         var x = 11
         let y = 22
-        console.log(x, y) // What is the expected output and why?
+        console.log(x, y) // Guess what the expected output is?
     }
-    console.log(x, y) // What is the expected output and why?
+    console.log(x, y) // Guess what the expected output is and why.
 }
 // testVarScope_2()
 
-//pass by value - primitiive data type
+//pass by value - primitive data type
 function updateValue(value){
     value = 1000
 }
 // let x = 100
 // updateValue(x)
-// console.log(x) // What is the expected output and why? 
+// console.log(x) // Guess what the expected output is and why.
 
 //pass by reference - user-defined object.
 function updateProperty(obj){
@@ -52,7 +52,7 @@ function updateProperty(obj){
 // let userObj = {user: "ABC"}
 // console.log(userObj) // What is the expected output
 // updateProperty(userObj)
-// console.log(userObj) // What is the expected output and why?
+// console.log(userObj) // Guess what the expected output is and why.
 
 function updateReference(obj){
     obj = {singer: "Freddie Mercury"}
@@ -60,7 +60,7 @@ function updateReference(obj){
 let newObj = {user: "ABC"}
 // console.log(newObj) // What is the expected output
 // updateReference(newObj)
-// console.log(newObj) // What is the expected output and why?
+// console.log(newObj) // Guess what the expected output is and why.
 
 function updateArrayElement(arr){
     arr[0] = 50
@@ -68,7 +68,7 @@ function updateArrayElement(arr){
 // numbers = [1, 2, 3, 4, 5]
 // console.log(numbers)
 // updateArrayElement(numbers)
-// console.log(numbers) // What is the expected output and why?
+// console.log(numbers) // Guess what the expected output is and why.
 
 function updateArray(arr){
     arr = [10, 20, 30, 40, 50]
